659617a8b690a399735f36f70f4867068183b128
PreciousChicken <hello@preciouschicken.com>
2020-07-20 13:12:22 +0100

    Duration changed to dtend

--- contracts/CalStore.sol
+++ contracts/CalStore.sol
@@ -7,7 +7,7 @@ contract CalStore  {
     struct VEvent {
         uint dtstamp;
         uint dtstart;
-        uint duration;
+        uint dtend;
         string summary;
         string description;
         uint uid; // This should by dtstart, plus an id, plus msg.owner Change to string eventually
@@ -16,10 +16,10 @@ contract CalStore  {
     VEvent[] private userVEvent;
     mapping(address => VEvent[]) private store;
 
-    function storeEvent(uint _dtstamp, uint _dtstart, uint _duration, string memory _summary, string memory _description) public {
+    function storeEvent(uint _dtstamp, uint _dtstart, uint _dtend, string memory _summary, string memory _description) public {
         VEvent[] memory currentData = store[msg.sender];
         uint nextId = currentData.length + 1;
-        VEvent memory newData = VEvent(_dtstamp, _dtstart, _duration, _summary, _description, nextId);
+        VEvent memory newData = VEvent(_dtstamp, _dtstart, _dtend, _summary, _description, nextId);
         userVEvent.push(newData);
         store[msg.sender] = userVEvent;
     }
@@ -30,12 +30,14 @@ contract CalStore  {
 
     // Returns all msg for msg.sender, regardless of time
     function getEvents(address _calOwner) public view returns (VEvent[] memory) {
+
+        // Return error if no events
         VEvent[] memory tempData = store[_calOwner];
         return tempData;
     }
 }
 
 // CalStore.deployed().then(function(instance) {app = instance})
-// app.storeEvent(1595170930, 1595171111, 33, "Deloitte Meeting", "Discuss new project");
-// app.getEvents('0xD6733F5011732cfb46788549bb8342556E2D9096');
+// app.storeEvent(1595170930, 1501419600, 1501421400, "Deloitte Meeting", "Discuss new project");
+// app.getEvents('0x5aE61985C224be414018795Aff6C60F4cA37A8fE');
 // app.justSayHi();
