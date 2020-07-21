ae5321b4220f93d9bbf6e9c6f785123590d33324
PreciousChicken <hello@preciouschicken.com>
2020-07-21 15:35:01 +0100

    Adding handlers

--- client/src/App.js
+++ client/src/App.js
@@ -5,40 +5,71 @@ import { Button, TextField } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardDateTimePicker } from '@material-ui/pickers';
 
 function App() {
-	const [selectedDate, handleDateChange] = useState(new Date());
+	const [selectedStartDate, handleStartDateChange] = useState(new Date());
+	const [selectedEndDate, handleEndDateChange] = useState(new Date());
+	const [eventTitle, setEventTitle] = "";
+	const [eventDescription, setEventDescription] = "";
 
 	// Handles user store message form submit
-	const handleStoreMsg = (e: React.FormEvent) => {
+	const handleNewEvent = (e: React.FormEvent) => {
+		console.log(selectedStartDate);
+		setEventTitle();
+		setEventDescription();
 		e.preventDefault();
 	};
-  return (
+	return (
 		<main>
 		<h1>Forget-me-Block: Ethereum Calendar</h1>
 
-			<form onSubmit={handleStoreMsg}>
+		<h2>New event:</h2>
 
+		<form onSubmit={handleNewEvent}>
+
+		<div className="block-element">
+		<TextField 
+		name="title" id="outlined-basic" label="Title" variant="outlined" />
+		</div>
 		<div className="block-element">
 		<MuiPickersUtilsProvider utils={DateFnsUtils}>	
 		<KeyboardDateTimePicker 
 		format="yyyy-MM-dd HH:mm"
 		ampm="false"
-		label="Date to store until:"
+		label="Start"
 		inputVariant="outlined"
-		value={selectedDate} onChange={handleDateChange} />
+		value={selectedStartDate} onChange={handleStartDateChange} />
 		</MuiPickersUtilsProvider>
 		</div>
 		<div className="block-element">
-		<TextField 
-		name="msgToStore" id="outlined-basic" label="Message to store:" variant="outlined" />
+		<MuiPickersUtilsProvider utils={DateFnsUtils}>	
+		<KeyboardDateTimePicker 
+		format="yyyy-MM-dd HH:mm"
+		ampm="false"
+		label="End"
+		inputVariant="outlined"
+		value={selectedEndDate} onChange={handleEndDateChange} />
+		</MuiPickersUtilsProvider>
+		</div>
+		<div className="block-element">
+		      <TextField
+          id="outlined-multiline-static"
+          label="Description"
+		style = {{width: 450}}
+          multiline
+          rows={4}
+          defaultValue=""
+          variant="outlined"
+        />
 		</div>
 		<div className="block-element">
 		<Button variant="contained" color="primary" type="submit">
 		Submit
 		</Button>
+
 		</div>
 		</form>
+		<h2>Generate iCal for import:</h2>
 		</main>
-  );
+	);
 }
 
 export default App;
